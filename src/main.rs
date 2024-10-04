10e249a4b39606cd98f72acbcb236fe3da69e493
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-04 17:26:51 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ fn main() { //declare main funciton
 
     let arraysum: i32 = array.iter().sum(); // decalre arraysum variable as 32 bit signed integer -> use array iter method to iterate through array and sum method to get sum
 
-    println!(
+    println!( //print to terminal sum of array filling {} -> arraysum variable
 
         "sum of array is {}",
-        arraysum
+        arraysum //fill arraysum -> {}
     );
 
 }
