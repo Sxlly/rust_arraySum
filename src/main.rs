842780e49941dd81dac0a4d54f652721fccc140e
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-04 17:25:15 +0800

    remove mut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+// Shae Sullivan
+// 20643731
+// Programming Languages - Assignment
+// 17/09/24
+
+
+fn main() { //declare main funciton 
+
+    let array: [i32; 5] = [10,20,30,40,50]; //declare array with all contents having a 32 bit signed integer data type and 5 index length
+
+    let arraysum: i32 = array.iter().sum(); // decalre arraysum variable as 32 bit signed integer -> use array iter method to iterate through array and sum method to get sum
+
+    println!(
+
+        "sum of array is {}",
+        arraysum
+    );
+
 }
